845ae1e223c5dc66619ec218249f5da68b80a809
WallRose <164656132+WallRose@users.noreply.github.com>
2024-04-03 21:24:23 +0200

    Add files via upload

--- BuckOptimizer.m
+++ BuckOptimizer.m
@@ -1,50 +1,21 @@
-clc
-clear;
-close
-
+clc;clear all;close all;
 %Imposto i nomi dei path dei vari file
-spicePath = 'C:\Users\Filippo\AppData\Local\Programs\ADI\LTspice\LTspice.exe'; % This is the path to your LT Spice installation
-filePath = 'C:\Users\Filippo\Documents\LTspice\'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
-fileName = 'Buck_opt'; %netlist to be optimized
-RunLTstring = sprintf('"%s" -b "%s%s.net"',spicePath, filePath, fileName); %Comando per lanciare la simulazione della netlist
-
+% sim.spicePath = 'C:\Users\Filippo\AppData\Local\Programs\ADI\LTspice\LTspice.exe'; % This is the path to your LT Spice installation
+sim.spicePath = 'C:\Program Files\LTC\LTspiceXVII\XVIIx64.exe'; % This is the path to your LT Spice installation
+% sim.filePath = 'C:\Users\Filippo\Documents\LTspice\'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
+sim.filePath = 'C:\Users\franz\Desktop\MATLABCircuitOptimizer-main\'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
+sim.fileName = 'Buck_opt'; %netlist to be optimized
+sim.RunLTstring = sprintf('"%s" -b "%s%s.net"',sim.spicePath, sim.filePath, sim.fileName); %Comando per lanciare la simulazione della netlist
+sim.netlist_filename = [sim.filePath 'Buck.net']; %original netlist with param
+% looks like buck.net autodelete.
 %Mos parameter to be opt
 W1 = 8e-3; %default
 W2 = 8e-3; %default
-
-%Prova di letturadella netlist (e sostituzione parametri W mos1 e W mos2)
-netlist_filename = strcat(filePath,"Buck.net"); %original netlist with param
-h = fopen(netlist_filename,'r');
-Testo=fread(h,Inf,'*char')';
-Testo=strrep(Testo,'$W1',num2str(W1)); %Prova W = 10mm
-Testo=strrep(Testo,'$W2',num2str(W2)); %Prova W = 10mm
-fclose(h);
-netlist_filename = strcat(filePath,fileName,".net");
-hdest=fopen(netlist_filename, 'w');
-fwrite(hdest,Testo,'char');
-fclose(hdest);
-
-%Prova di simulazione della netlist
-[status,cmdout] = system(RunLTstring);
-if(status) 
-    fprintf('ERROR, LTSpice sim failed to run.\n ');
-    return;
-end
-
-
-%%Read results (in this case only efficiency, but later must be optimized
-%%Area)
-output_filename = strcat(filePath, fileName,".log");
-fileID = fopen(output_filename);
-
-Testo=fread(fileID,Inf,'*char')';
-fclose(fileID);
-wordString = 'eff: pout/pin=';
-newStr = extractAfter(Testo,wordString);
-Eff = sscanf(newStr,'%f')
-W1,W2
-
-%Prova di ottimizzazione
-
-%Calcolo della fitness function
-% x = particleswarm(fun,nvars) %Fun dovrà essere una fitness function
+val=[W1,W2];
+%fitness custom handle function
+fit= @(x) fitness(x,sim);
+fit(val)
+%%
+options = optimoptions('particleswarm','Display','Iter','SwarmSize',80);
+% PSO optimizer 
+[x_sol,f_sol,~,~] = particleswarm(fit,2,[1e-5; 1e-5],[1e-2; 1e-2],options);
---
+++ fitness.m
@@ -0,0 +1,28 @@
+function eff = fitness(features,sim)
+%FITNESS Summary of this function goes here
+% Open default netlist
+h = fopen(sim.netlist_filename,'r');
+Testo=fread(h,Inf,'*char')';
+% replace features
+Testo=strrep(Testo,'$w1',num2str(features(1))); 
+Testo=strrep(Testo,'$w2',num2str(features(2))); 
+fclose(h);
+% Write netlist to be simulated
+hdest=fopen([sim.filePath sim.fileName '.net'], 'w');
+fwrite(hdest,Testo,'char');
+fclose(hdest);
+% Netlist simulation
+[status,~] = system(sim.RunLTstring);
+if(status) 
+    fprintf('ERROR, LTSpice sim failed to run.\n ');
+    return;
+end
+% Read efficiency
+fileID = fopen([sim.filePath, sim.fileName,'.log']);
+Testo=fread(fileID,Inf,'*char')';
+fclose(fileID);
+newStr = extractAfter(Testo,'eff: pout/pin=');
+% minus the efficiency to minimize (max to min problem)
+eff = -sscanf(newStr,'%f');
+end
+
