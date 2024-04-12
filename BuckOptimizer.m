7e3116788bd890cff2c378cbdd66d3560e3ff536
WallRose <164656132+WallRose@users.noreply.github.com>
2024-04-12 23:32:46 +0200

    Update loop schematics and fitness function

--- BuckOptimizer.m
+++ BuckOptimizer.m
@@ -1,21 +1,21 @@
 clc;clear all;close all;
 %Imposto i nomi dei path dei vari file
-sim.spicePath = 'C:\Users\Filippo\AppData\Local\Programs\ADI\LTspice\LTspice.exe'; % This is the path to your LT Spice installation
-% sim.spicePath = 'C:\Program Files\LTC\LTspiceXVII\XVIIx64.exe'; % This is the path to your LT Spice installation
-sim.filePath = 'C:\Users\Filippo\Documents\LTspice\'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
-% sim.filePath = 'C:\Users\franz\Desktop\MATLABCircuitOptimizer-main\'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
-sim.fileName = 'Buck_opt'; %netlist to be optimized
-sim.RunLTstring = sprintf('"%s" -b "%s%s.net"',sim.spicePath, sim.filePath, sim.fileName); %Comando per lanciare la simulazione della netlist
-sim.netlist_filename = [sim.filePath 'Buck.net']; %original netlist with param
-% looks like buck.net autodelete.
+sim.filePath = 'C:\Users\Kreidos\Desktop\MATLABCircuitOptimizer-loop'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
+sim.fileName='NetBuck.net'; % 'NetBuck_Vopt.net'
+sim.netlist_filename = [sim.filePath '\' sim.fileName]; %original netlist with param to be optimized
+sim.Vtarget=12;
 %Mos parameter to be opt
-W1 = 8e-3; %default
-W2 = 8e-3; %default
-val=[W1,W2];
+W1 = 1e-2; %default
+W2 = 1e-2; %default
 %fitness custom handle function
 fit= @(x) fitness(x,sim);
-fit(val)
+fit([W1 W2])
+% another version to change the Vout, not tested
+% Rf=8.03e3;
+% fitVpopt= @(x) fitnessVopt(x);
+% fit(Rf)
 %%
-options = optimoptions('particleswarm','Display','Iter','SwarmSize',80);
+options = optimoptions('particleswarm','Display','Iter','SwarmSize',40,'MaxStallIterations',5,'UseParallel',false);
 % PSO optimizer 
-[x_sol,f_sol,~,~] = particleswarm(fit,2,[10e-3; 10e-3],[25e-3; 25e-3],options);
+[x_sol,f_sol,~,~] = particleswarm(fit,2,[1e-3; 1e-3],[25e-3; 25e-3],options);
+% [x_sol,f_sol,~,~] = particleswarm(fitVpopt,2,1e3,113e3,options);
--- fitnessVopt.m
+++ fitnessVopt.m
@@ -1,17 +1,14 @@
-function eff = fitness(features,sim)
+function cost = fitnessVopt(features)
 %FITNESS Summary of this function goes here
 % Open a default netlist
-h = fopen(sim.netlist_filename,'r');
+h = fopen('C:\Users\Kreidos\Desktop\MATLABCircuitOptimizer-loop\NetBuck_Vopt.net','r');
 Testo=fread(h,Inf,'*char')';
 % Take the txt and replace the special characters on the default netlist (the features to be optimized)
-Testo=strrep(Testo,'$w1',num2str(features(1))); 
-Testo=strrep(Testo,'$w2',num2str(features(2)));
-Testo=strrep(Testo,'$Rd_HS',num2str(.04*features(1))); 
-Testo=strrep(Testo,'$Rd_LS',num2str(.04*features(2))); 
+Testo=strrep(Testo,'$Rf',num2str(features)); 
 fclose(h);
 % Write the text as netlist to be simulated. Default file is not modified.
 % Generate random name for the written netlist to avoid conflict wrt parallel workers
-pathtmp=tempname(sim.filePath);
+pathtmp=tempname('C:\Users\Kreidos\Desktop\MATLABCircuitOptimizer-loop');
 % Write netlist to be simulated
 hdest=fopen([pathtmp '.net'], 'w');
 fwrite(hdest,Testo,'char');
@@ -27,9 +24,10 @@ end
 fileID = fopen([pathtmp '.log']);
 Testo=fread(fileID,Inf,'*char')';
 fclose(fileID);
-newStr = extractAfter(Testo,'eff: pout/pin=');
+newStr = extractAfter(Testo,'AVG(v(out))=');
 % minus the efficiency is the cost to be minimized with PSO optimizer (efficiency is maximized)
-eff = -sscanf(newStr,'%f');
+Vout = sscanf(newStr,'%f');
+cost= abs(Vout-12);
 % Clean tmp files
 delete([pathtmp '.net'])
 delete([pathtmp '.log'])
