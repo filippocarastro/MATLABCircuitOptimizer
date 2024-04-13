b746fc769e3177f222346827f7a257576c9916a1
WallRose <164656132+WallRose@users.noreply.github.com>
2024-04-13 17:12:37 +0200

    Vout Controller
    Fitness evaluate abs(Vout-Vtarget), returns the DC value of the controller required to achieve that. Max 16 volt reachable with that circuit and components.

--- BuckOptimizer.m
+++ BuckOptimizer.m
@@ -1,22 +1,21 @@
 clc;clear all;close all;
 %Imposto i nomi dei path dei vari file
-% sim.spicePath = 'C:\Users\Filippo\AppData\Local\Programs\ADI\LTspice\LTspice.exe'; % This is the path to your LT Spice installation
-% sim.spicePath = 'C:\Program Files\LTC\LTspiceXVII\XVIIx64.exe'; % This is the path to your LT Spice installation
-% % sim.filePath = 'C:\Users\Filippo\Documents\LTspice\'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
-% sim.filePath = 'C:\Users\Kreidos\Desktop\MATLABCircuitOptimizer-main\'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
-% sim.fileName = 'Buck_opt'; %netlist to be optimized
-% sim.RunLTstring = sprintf('"%s" -b "%s%s.net"',sim.spicePath, sim.filePath, sim.fileName); %Comando per lanciare la simulazione della netlist
-% sim.netlist_filename = [sim.filePath 'Buck.net']; %original netlist with param
-% % looks like buck.net autodelete.
-% %Mos parameter to be opt
-% W1 = 8e-3; %default
-% W2 = 8e-3; %default
-% val=[W1,W2];
+sim.filePath = 'C:\Users\Kreidos\Desktop\MATLABCircuitOptimizer-loop'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
+sim.fileName='NetBuck.net'; % 'NetBuck_Vopt.net'
+sim.netlist_filename = [sim.filePath '\' sim.fileName]; %original netlist with param to be optimized
+sim.Vtarget=12;
+%Mos parameter to be opt
+W1 = 1e-2; %default
+W2 = 1e-2; %default
 %fitness custom handle function
-fit= @(x) fitness(x);
-% fit(val)
-options = optimoptions('particleswarm','UseParallel',false,'MaxStallIterations',5,'Display','Iter','SwarmSize',40);
+fit= @(x) fitness(x,sim);
+fit([W1 W2])
+% another version to change the Vout, not tested
+% Rf=8.03e3;
+% fitVpopt= @(x) fitnessVopt(x);
+% fit(Rf)
+%%
+options = optimoptions('particleswarm','Display','Iter','SwarmSize',40,'MaxStallIterations',5,'UseParallel',false);
 % PSO optimizer 
-tic
-[x_sol,f_sol,~,~] = particleswarm(fit,2,[1e-5; 1e-5],[1e-2; 1e-2],options);
-toc
+[x_sol,f_sol,~,~] = particleswarm(fit,2,[1e-3; 1e-3],[25e-3; 25e-3],options);
+% [x_sol,f_sol,~,~] = particleswarm(fitVpopt,2,1e3,113e3,options);
