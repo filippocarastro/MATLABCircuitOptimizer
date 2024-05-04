735cfd8532821f75cba516ac435571d825c1c252
filippocarastro <filippo.carastro@icloud.com>
2024-05-04 14:44:23 +0200

    Last update

--- BuckOptimizer.m
+++ BuckOptimizer.m
@@ -1,22 +1,22 @@
-clc;clear all;close all;
+clc
+clear
+close all
 %Imposto i nomi dei path dei vari file
+sim.filePath = 'C:\Users\Filippo\Documents\LTspice'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
 sim.spicePath = 'C:\Users\Filippo\AppData\Local\Programs\ADI\LTspice\LTspice.exe'; % This is the path to your LT Spice installation
-% sim.spicePath = 'C:\Program Files\LTC\LTspiceXVII\XVIIx64.exe'; % This is the path to your LT Spice installation
-sim.filePath = 'C:\Users\Filippo\Documents\LTspice\'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
-% sim.filePath = 'C:\Users\franz\Desktop\MATLABCircuitOptimizer-main\'; %This is the path to the working LTSPICE folder (schems, netlists, simulation output files)
-sim.fileName = 'Buck_opt'; %netlist to be optimized
-sim.RunLTstring = sprintf('"%s" -b "%s%s.net"',sim.spicePath, sim.filePath, sim.fileName); %Comando per lanciare la simulazione della netlist
-sim.netlist_filename = [sim.filePath 'Buck.net']; %original netlist with param
-sim.LTSpice_outputfile = sprintf('%s%s.raw', sim.filePath,sim.fileName);
-% looks like buck.net autodelete.
+sim.fileName='Buck.net';
+sim.netlist_filename = [sim.filePath '\' sim.fileName]; %original netlist with param to be optimized
 %Mos parameter to be opt
-W1 = 84500; %default [mm]
-W2 = 154500; %default [mm]
+W1 = 1545000; %default [mm]
+W2 = 1545000; %default [mm]
 val=[W1,W2];
 %fitness custom handle function
 fit= @(x) fitness(x,sim);
-fit(val)
+fit([W1 W2])
+
 %%
-options = optimoptions('particleswarm','Display','Iter','SwarmSize',10,'MaxIterations',5);
+% parpool('local', 2);
+% options = optimoptions('particleswarm','Display','Iter','SwarmSize',15,'MaxIterations',10,'MaxStallIterations',3,'UseParallel',true);
+options = optimoptions('particleswarm','Display','Iter','SwarmSize',15,'MaxIterations',10,'MaxStallIterations',3,'UseParallel',false);
 % PSO optimizer 
-[x_sol,f_sol,~,~] = particleswarm(fit,2,[84500; 84500],[154500; 154500],options);
+[x_sol,f_sol,~,~] = particleswarm(fit,2,[84500; 84500],[1545000; 1545000],options);
--- fitness.m
+++ fitness.m
@@ -1,42 +1,41 @@
 function eff = fitness(features,sim)
 %FITNESS Summary of this function goes here
-% Open default netlist
+% Open a default netlist
 h = fopen(sim.netlist_filename,'r');
 Testo=fread(h,Inf,'*char')';
-% replace features
+% Take the txt and replace the special characters on the default netlist (the features to be optimized)
 Testo=strrep(Testo,'$w1',num2str(features(1))); 
 Testo=strrep(Testo,'$w2',num2str(features(2)));
-Testo=strrep(Testo,'$Rd_HS',num2str(0.25*features(1))); 
-Testo=strrep(Testo,'$Rd_LS',num2str(0.25*features(2))); 
-fprintf('W_HS=%f\tW_LS=%f\n',features(1),features(2));
+Testo=strrep(Testo,'$Rd_HS',num2str(.04*features(1))); 
+Testo=strrep(Testo,'$Rd_LS',num2str(.04*features(2))); 
 fclose(h);
+% Write the text as netlist to be simulated. Default file is not modified.
+% Generate random name for the written netlist to avoid conflict wrt parallel workers
+pathtmp=tempname(sim.filePath);
 % Write netlist to be simulated
-hdest=fopen([sim.filePath sim.fileName '.net'], 'w');
+hdest=fopen([pathtmp '.net'], 'w');
 fwrite(hdest,Testo,'char');
 fclose(hdest);
-% Netlist simulation
-[status,~] = system(sim.RunLTstring);
-if(status) 
+% Netlist simulation with system (cmd) launching the command string related to the written netlist
+[status,~] = system([sim.spicePath,' -b ',pathtmp,'.net']); 
+disp(status);
+% Debug status error for the current launched LTSpice simulation.
+if status 
     fprintf('ERROR, LTSpice sim failed to run.\n ');
-    return;
+    disp(features);
 end
-% Read efficiency
-fileID = fopen([sim.filePath, sim.fileName,'.log']);
+% Read efficiency (fitness cost) from log file
+fileID = fopen([pathtmp '.log']);
 Testo=fread(fileID,Inf,'*char')';
 fclose(fileID);
 newStr = extractAfter(Testo,'eff: pout/pin=');
-
-% minus the efficiency to minimize (max to min problem)
-eff = -sscanf(newStr,'%f')
-
-% result = LTspice2Matlab(sim.LTSpice_outputfile); % parse output file
-% Vout=result.variable_mat(12,:);
-% Iout=result.variable_mat(36,:);
-% Vin = result.variable_mat(1,:);
-% IVin = result.variable_mat(39,:);
-% Pout = mean(Vout.*Iout);
-% Pin = mean(Vin.*IVin);
-% eff = Pout/Pin
-
+% minus the efficiency is the cost to be minimized with PSO optimizer (efficiency is maximized)
+eff = -sscanf(newStr,'%f');
+disp(-eff);
+% Clean tmp files
+delete([pathtmp '.net'])
+delete([pathtmp '.log'])
+delete([pathtmp '.raw'])
+delete([pathtmp '.op.raw'])
 end
 
